ce4173f1f0b51132659d0897ebc2f6e59c9e60db
maksim <6893910@stud.nau.edu.ua>
2023-10-26 14:29:02 +0300

    commit 1.1

--- src/main/java/model/dao/Connection/Connector.java
+++ src/main/java/model/dao/Connection/Connector.java
@@ -1,4 +1,24 @@
 package model.dao.Connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Connector {
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://localhost:5432/provider";
+        String user = "maksim";
+        String password = "9112";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            String sql = "SELECT * FROM users_t";
+            try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                System.out.println(statement.getS);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
